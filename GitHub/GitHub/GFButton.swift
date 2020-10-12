9fe4563c25c2be71fe25117cc3af9f0f1f6bb394
Daniil <Danilin-Stati@yandex.ru>
2020-10-12 22:30:55 +0300

    Make custom views

---
+++ GitHub/GitHub/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GitHub
+//
+//  Created by Даниил Статиев on 12.10.2020.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
