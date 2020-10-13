fbb7a5fda5b188ed292b2b4a7035c98f259d99c9
Daniil <Danilin-Stati@yandex.ru>
2020-10-13 22:56:46 +0300

    Create custom alert

--- GitHub/GitHub/SearchVC.swift
+++ GitHub/GitHub/SearchVC.swift
@@ -40,7 +40,7 @@ class SearchVC: UIViewController {
     @ objc func pushFollowerListVC() {
         
         guard isUsernameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. Weneed to know who to look for 🤪.", buttonTitle: "Ok")
             return
         }
         
---
+++ GitHub/GitHub/UIViewController + Extention.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController + Extention.swift
+//  GitHub
+//
+//  Created by Даниил Статиев on 13.10.2020.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = CFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
