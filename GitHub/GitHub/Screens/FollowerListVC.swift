3538249d420c185f0c010c6547d61d97b498393c
Daniil <Danilin-Stati@yandex.ru>
2020-10-17 15:40:35 +0300

    Make loading view

--- GitHub/GitHub/Screens/FollowerListVC.swift
+++ GitHub/GitHub/Screens/FollowerListVC.swift
@@ -50,8 +50,11 @@ class FollowerListVC: UIViewController {
     }
     
     fileprivate func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: userName, page: page) { [weak self] result in
+            
             guard let self = self else { return }
+            self.dissmissLoadingView()
             
             switch result {
             case .success(let followers):
