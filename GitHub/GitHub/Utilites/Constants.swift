1cbdd82af2f43cc097b539734b8be0afd1e78e95
Daniil <Danilin-Stati@yandex.ru>
2020-10-19 20:28:04 +0300

    Make user info header

---
+++ GitHub/GitHub/Utilites/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHub
+//
+//  Created by Даниил Статиев on 19.10.2020.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
